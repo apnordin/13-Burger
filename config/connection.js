f3787e5a525887c6cdca5de1d862e9c3a47e72cd
Alex Nordin <apnordin@gmail.com>
2020-12-03 00:28:08 -0500

    index fully working

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "New1river!",
+    password: "",
     database: "burgers_db"
 });
 
