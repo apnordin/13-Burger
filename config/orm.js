ec7ccbd7eb2a0f5923c8552912f1db7886cec80c
Alex Nordin <apnordin@gmail.com>
2020-12-03 17:10:09 -0500

    Heroku

--- config/orm.js
+++ config/orm.js
@@ -15,7 +15,7 @@ var orm = {
 
     selectAll: function (tableInput, cb) {
 
-        var queryString = "SELECT * FROM burgers;";
+        var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
