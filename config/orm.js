193402c65728544f9bdaa9567f9ac9c99a5ff037
Alex Nordin <apnordin@gmail.com>
2020-12-03 16:48:29 -0500

    More heroku troubleshooting

--- config/orm.js
+++ config/orm.js
@@ -15,7 +15,7 @@ var orm = {
 
     selectAll: function (tableInput, cb) {
 
-        var queryString = "SELECT * FROM " + tableInput + ";";
+        var queryString = "SELECT * FROM burgers;";
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
