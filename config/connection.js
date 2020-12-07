7fbe85c24f096244fbb2777fedd03fceea9260e8
Alex Nordin <apnordin@gmail.com>
2020-12-07 11:55:12 -0500

    readme

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "New1river!",
+        password: "",
         database: "burgers_db"
     });
 };
